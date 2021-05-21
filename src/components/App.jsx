961bd287c803317dea0a304ef0f5a963412602b7
Nikolay Rademacher <hrrdx33@gmail.com>
2021-05-21 23:58:06 +0200

    feat: implement dragndrop frontend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone-uploader';
+import 'react-dropzone-uploader/dist/styles.css';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const App = () => (
-  <h1 class="grid place-items-center min-h-screen text-8xl font-light text-indigo-800">
-    Hello, world.
-  </h1>
-);
+const App = () => {
+  const [copyLinks, setCopyLinks] = useState([]);
+
+  // specify upload params and API url to file upload
+  const getUploadParams = ({ file }) => {
+    const body = new FormData();
+    body.append('dataFiles', file);
+    return {
+      url: 'https://dry-shore-64957.herokuapp.com/images/postImages',
+      body,
+    };
+  };
+
+  // handle the status of the file upload
+  const handleChangeStatus = ({ xhr }) => {
+    if (xhr) {
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          const result = JSON.parse(xhr.response);
+          setCopyLinks((prev) => [
+            ...prev,
+            { value: result.data.imageUrl, copied: false },
+          ]);
+        }
+      };
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1 className="py-4 text-center font-bold text-xl">Image Uploader (JPG & PNG)</h1>
+      <section>
+        <Dropzone
+          getUploadParams={getUploadParams}
+          onChangeStatus={handleChangeStatus}
+          styles={{
+            dropzone: {
+              overflow: 'auto',
+              border: '1px solid #999',
+              background: '#f5f5f5',
+            },
+            inputLabelWithFiles: { margin: '20px 3%' },
+          }}
+          canRemove={false}
+          accept="image/*"
+        />
+      </section>
+      <section className="p-4 flex flex-col items-center justify-center">
+        {copyLinks.map((link, i) => {
+          return (
+            <>
+              <a href={link.value} target="_blank"><img src={link.value} alt={`image ${i + 1}`} className="rounded-md mb-2 max-w-xl" /></a>
+              <article key={link.value} className="mb-4">
+                <span className="p-2 text-xs bg-gray-100 border-2 border-gray-200 rounded mr-2">
+                  {link.value}
+                </span>
+                <CopyToClipboard
+                  text={link.value}
+                  onCopy={() => {
+                    link.copied = true;
+                    setCopyLinks(copyLinks);
+                  }}
+                >
+                  <button className="p-2 text-sm text-[#2484ff] font-semibold bg-[#e6e6e6] rounded transition-all duration-200 hover:bg-[#2484ff] hover:text-[#e6e6e6]">
+                    Copy to clipboard
+                  </button>
+                </CopyToClipboard>
+              </article>
+            </>
+          );
+        })}
+      </section>
+    </div>
+  );
+};
 
 export default App;

7ce6bd06bc714d10752d664c3f375a28b72d4a3d
Nikolay Rademacher <hrrdx33@gmail.com>
2021-05-22 00:29:05 +0200

    fix: clipboard links in mobile view

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import 'react-dropzone-uploader/dist/styles.css';
+// import 'react-dropzone-uploader/dist/styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
